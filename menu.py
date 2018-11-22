f998bfacfbc9dc27dc20397dbb4c6438acb82689
triktron <ele.trik@hotmail.com>
2018-11-22 14:35:32 +0100

    added the timer

--- menu.py
+++ menu.py
@@ -16,6 +16,8 @@ import os
 import sys
 import re
 import importlib
+from threading import Timer
+import time
 
 def load_plugins():
     pysearchre = re.compile('.py$', re.IGNORECASE)
@@ -35,9 +37,11 @@ def load_plugins():
 
 lastAppId = 0
 menu = None
+menuSidebar = None
 currentApp = None
 
 bgColor = "#211535"
+bgColorSidebar = "#4a3172"
 
 def toggleHome():
     global menu
@@ -50,9 +54,11 @@ def toggleHome():
     if currentApp.panel.IsShown():
         currentApp.panel.Hide()
         menu.Show()
+        menuSidebar.Show()
     else:
         currentApp.panel.Show()
         menu.Hide()
+        menuSidebar.Hide()
 
 def addApp(app, panel):
     global lastAppId
@@ -80,21 +86,64 @@ def onKeyPress(event):
         event.Skip()
         toggleHome()
 
-def OnPaint(event):
-        global menu
-        dc = wx.PaintDC(menu)
-        dc.Clear()
-        dc.SetPen(wx.Pen('#d4d4d4'))
-        dc.SetBrush(wx.Brush('#c56c00'))
-        dc.DrawRectangle(0, 0, 480, 80)
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+timeElement = None
+timerThread = None
+def setupSidebar(panel, frame):
+    global timeElement
+    global timerThread
+    timeElement = wx.StaticText(panel, id=wx.ID_ANY, label=time.strftime('%H:%M'), pos=(9,400))
+    font = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+    timeElement.SetFont(font)
+    timeElement.SetForegroundColour((255,255,255))
+    timerThread = RepeatedTimer(1, updateTime)
+    frame.Bind(wx.EVT_CLOSE, OnClose)
+
+def OnClose(evnt):
+    global timerThread
+    timerThread.stop()
+    print("exit")
+    wx.GetApp().ExitMainLoop()
+
+def updateTime():
+    global timeElement
+    timeElement.SetLabel(time.strftime('%H:%M'))
 
 def init(frame):
     global menu
-    menu = wx.Panel(frame, wx.ID_ANY, size=(800,480))
+    global menuSidebar
+    menu = wx.Panel(frame, wx.ID_ANY, size=(720,480), pos=(80,0))
     menu.SetBackgroundColour(bgColor)
+    menuSidebar = wx.Panel(frame, wx.ID_ANY, size=(80,480))
+    menuSidebar.SetBackgroundColour(bgColorSidebar)
+    setupSidebar(menuSidebar, frame)
     menu.Show()
+    menuSidebar.Show()
     frame.Bind(wx.EVT_CHAR_HOOK, onKeyPress)
-    # menu.Bind(wx.EVT_PAINT, OnPaint)
     modules = load_plugins()
     for f in modules:
         if hasattr(f, 'init'):
