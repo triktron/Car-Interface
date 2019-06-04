66d6c5f392810bd7f6deff41725d5ea1195e8013
Vanesche Luca <luca.vanesche@leerling.pti.be>
2019-06-04 11:26:25 +0200

    removed the roughe files

--- build.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\vaneschel\Python37\python.exe -m PyInstaller --hidden-import=cefpython3 --hidden-import=wx.lib.agw.speedmeter --hidden-import=json --add-data apps;apps --add-data icons;icons --add-data libs;libs --add-data services;services --onefile main.py
-rmdir build
